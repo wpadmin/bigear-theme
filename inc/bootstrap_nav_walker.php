0c45dab2f11290e3bc48db0233acecc11904deb7
wpadmin <karskiy@gmail.com>
2025-06-04 23:04:07 +0300

    Удалена ссылка на название из десктоп меню

--- inc/bootstrap_nav_walker.php
+++ inc/bootstrap_nav_walker.php
@@ -183,9 +183,6 @@ function bootstrap_navbar_menu() {
     <!-- Десктопная версия меню -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary d-none d-lg-block">
         <div class="container-fluid">
-            <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">
-                <?php echo get_bloginfo('name'); ?>
-            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
